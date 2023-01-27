0ed0894ede49a8e46bcddb283c63e9c628409010
MilanNayak7 <tarunnayak073@gmail.com>
2023-01-27 13:05:07 +0530

    signup complete

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2'
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  constructor(private userService:UserService,private snack:MatSnackBar){}
+
+  public user = {
+    userName:'',
+    password:'',
+    firstName:'',
+    lastName:'',
+    email:'',
+    phone:''
+  };
+
+  formSubmit(){
+    
+    console.log(this.user);
+    if(this.user.userName == '' || this.user.userName == null){
+      this.snack.open('Username is required !!','ok',{
+        duration:3000,
+        verticalPosition:'top',
+        horizontalPosition:'left'
+      })
+      return;
+    }
+
+    this.userService.addUser(this.user).subscribe(
+      (data:any)=>{
+        Swal.fire('Success','User is registered with id'+data.id,'success');
+      },
+      (error)=>{
+        console.log(error)
+        alert('something went wrong');
+      }
+    )
+
+  }
+
+}
